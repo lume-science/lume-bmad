73a2b49435e15ce717e64616be4b4760e7edab1f
Ryan Roussel <rroussel@slac.stanford.edu>
2026-02-20 16:00:17 -0600

    remove slac-specific code

--- lume_bmad/utils.py
+++ lume_bmad/utils.py
@@ -40,51 +40,6 @@ TAO_OUTPUT_UNITS = {
 ################################################################
 
 
-def import_control_variables(control_variable_file: str):
-    """
-    Import control variables from a YAML file and define them as ScalarVariables.
-    Also get the mapping between device PV names and Bmad element names.
-
-    TODO: move SLAC specific mapping and unit conversions to slac-tools
-
-    Parameters
-    ----------
-    control_variable_file: str
-        Path to the YAML file containing control variable definitions.
-
-    Returns
-    -------
-    dict[str, ScalarVariable]
-        Dictionary of pv variables to ScalarVariable instances.
-    dict[str, str]
-        Mapping between PV names and Bmad element names + attributes.
-    """
-
-    control_name_to_bmad = {}
-    var_dict = {}
-
-    with open(control_variable_file, "r") as file:
-        control_variable = yaml.safe_load(file)
-
-    # handle quadrupoles
-    quads = control_variable.get("quad", [])
-    for quad in quads:
-        pv_name = quad["pvname"]
-
-        # map pv to bmad element name and attribute
-        control_name_to_bmad[pv_name] = " ".join(
-            [quad["bmad_name"], quad["bmad_attribute"]]
-        )
-        var_dict[pv_name] = ScalarVariable(
-            name=pv_name,
-            value_range=(quad["min_value"], quad["max_value"]),
-            unit="kG-m",
-            read_only=False,
-        )
-
-    return var_dict, control_name_to_bmad
-
-
 def import_output_variables(output_variable_file: str):
     """
     Import output variables from a YAML file and define them as ScalarVariables.
@@ -119,80 +74,6 @@ def import_output_variables(output_variable_file: str):
             )
 
     return out_dict
-
-
-###############################################################
-# Utility classes / functions for Bmad/Tao interaction
-###############################################################
-
-
-class SLAC2BmadTransformer(BmadTransformer):
-    def get_tao_property(self, tao: Tao, control_name: str):
-        """
-        Get a property of an element from Bmad via Tao and
-        return its value in control system (EPICS) units.
-
-        # TODO: implment other variable types as needed,
-        # utilize future datamaps functionality from lcls-live or database
-
-        Parameters
-        ----------
-        tao: Tao
-            Instance of the Tao class.
-        control_name: str
-            Name of the control variable to retrieve.
-
-        Returns
-        -------
-        Any
-            Value of the requested property.
-
-        """
-
-        # Map control name to element and attribute
-        element, attr = self.control_name_to_bmad[control_name].split(" ")
-        ele_attr = tao.ele_gen_attribs(element)
-        if attr == "b1_gradient":
-            # convert from Bmad units to EPICS units
-            return ele_attr["B1_GRADIENT"] * ele_attr["L"] * 10
-        else:
-            return ele_attr[attr]
-
-    def get_tao_commands(
-        self, tao: Tao, pvdata: dict[str, Any], beam_path: str
-    ) -> list[str]:
-        """
-        Get Tao commands to set a property of an element in Bmad via Tao. Handle
-        mapping control names to element attributes and any necessary unit conversions as needed.
-
-        Parameters
-        ----------
-        tao: Tao
-            Instance of the Tao class.
-        pvdata: dict[str, Any]
-            Dictionary of control variable names and values to set
-        beam_path: str
-            Beam path in the Bmad lattice (e.g. "cu_hxr")
-
-        Returns
-        -------
-        list[str]
-            List of Tao commands to execute
-
-        """
-        tao_cmds = []
-        for name, value in pvdata.items():
-            element, attr = self.control_name_to_bmad[name].split(" ")
-            if attr == "b1_gradient":
-                # convert from EPICS units to Bmad units
-                ele_attr = tao.ele_gen_attribs(element)
-                bmad_value = value / (ele_attr["L"] * 10)
-            else:
-                bmad_value = value
-            tao_cmd = f"set ele {element} {attr} = {bmad_value}"
-            tao_cmds.append(tao_cmd)
-
-        return tao_cmds
 
 
 def evaluate_tao(tao: Tao, tao_cmds: list[str]) -> None:
