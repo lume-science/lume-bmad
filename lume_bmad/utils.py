c56607377804cc14f4c0859e690b2955594db482
Ryan Roussel <rroussel@slac.stanford.edu>
2026-03-11 16:27:10 -0500

    Revert "remove depreciated methods"
    This reverts commit d3ae1acba337489e9dfd2c63994751b617a1fe8d.

--- lume_bmad/utils.py
+++ lume_bmad/utils.py
@@ -35,6 +35,45 @@ TAO_OUTPUT_UNITS = {
     "vec0": "m",
 }
 
+###############################################################
+# Utility functions for importing control and output variables
+################################################################
+
+def import_output_variables(output_variable_file: str):
+    """
+    Import output variables from a YAML file and define them as Variable instances.
+    Note that output variables are read-only.
+
+    TODO: move SLAC specific mapping and unit conversions to slac-tools
+
+    Parameters
+    ----------
+    output_variable_file: str
+        Path to the YAML file containing output variable definitions.
+
+    Returns
+    -------
+    dict[str, Variable]
+        Dictionary of output variables mapped by their names.
+    """
+
+    out_dict = {}
+
+    with open(output_variable_file, "r") as file:
+        output_variables = yaml.safe_load(file)
+
+    for ele in output_variables.keys():
+        for attr in output_variables[ele].keys():
+            name = attr.replace("ele", "").replace(".", "_")
+            name = ele + name + "_"
+            out_dict[name] = ScalarVariable(
+                name=name,
+                unit=TAO_OUTPUT_UNITS[attr],
+                read_only=True,
+            )
+
+    return out_dict
+
 
 def evaluate_tao(tao: Tao, tao_cmds: list[str]) -> None:
     """
@@ -56,6 +95,61 @@ def evaluate_tao(tao: Tao, tao_cmds: list[str]) -> None:
     tao.cmd("set global lattice_calc_on = F")
     tao.cmds(tao_cmds)
     tao.cmd("set global lattice_calc_on = T")
+
+
+def get_tao_lat_list_outputs(tao: Tao) -> dict[str, list[Any]]:
+    """
+    Returns dictionary of Tao output values including element name, twiss and rmats at all elements.
+
+    Parameters
+    ----------
+    tao: Tao
+        Instance of the Tao class.
+
+    Returns
+    -------
+    dict[str, list[Any]]
+        Dictionary mapping output variable names to their values at each element in a lattice.
+    """
+    # populate output dictionary
+    outputs = {}
+    lattice_elements = tao.lat_list("*", "ele.name")
+    for k in TAO_OUTPUT_UNITS.keys():
+        output = tao.lat_list("*", "ele." + k)
+        outputs.update(
+            {
+                ele + k.replace("ele", ""): val
+                for ele, val in zip(lattice_elements, output)
+            }
+        )
+
+    return outputs
+
+
+def get_beam_info(tao: Tao) -> dict[str, list[Any]]:
+    """
+    Returns dictionary of beam tracking information
+    
+    Parameters
+    ----------
+    tao: Tao
+        Instance of the Tao class.
+
+    Returns
+    -------
+    dict[str, list[Any]]
+        Dictionary mapping beam tracking beam or single particle and beam saved at element list
+
+    """
+    beam_info = {}
+    lines = tao.cmd('python show beam')
+    track_type = [l.split('=') for l in lines if "global%track_type" in l][0][1]
+    beam_info['track_type'] =  track_type[2:-1]
+    saved_at = [l.split('=') for l in lines if "saved_at" in l][0][1]
+    saved_at = saved_at.strip(' "').split(',')
+    beam_info['saved_at'] = [s.strip(' ') for s in saved_at]
+
+    return beam_info
 
 
 def get_particle_group_at_element(tao:Tao, element, file_name=None):
