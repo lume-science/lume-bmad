e8ec87f843b632a38339b99351ca58d668fcf32b
William Colocho <colocho@stanford.edu>
2026-03-26 08:14:40 -0700

    Reverting changes for old pytao version

--- lume_bmad/utils.py
+++ lume_bmad/utils.py
@@ -9,7 +9,7 @@ from pmd_beamphysics import ParticleGroup
 
 
 TAO_OUTPUT_UNITS = {
-    "name":"",
+    "name": "",
     "ix_ele": "",
     "ix_branch": "",
     "a.beta": "m",
@@ -151,22 +151,6 @@ def get_beam_info(tao: Tao) -> dict[str, list[Any]]:
     beam_info['saved_at'] = [s.strip(' ') for s in saved_at]
 
     return beam_info
-
-
-def get_particle_group_at_element(tao:Tao, element, file_name=None):
-    """
-    returns a ParticleGroup and optionally writes Bmad particles to file
-    """
-    parameters =  \
-        ["x", "px", "y", "py", "z", "pz", "p0c", "charge", "state", "t", "status"]
-    data_bunch1 = {"species": "electron"}
-    for par in parameters:
-        data_bunch1[par] = tao.bunch1(element, par)
-    P = ParticleGroup.from_bmad(data_bunch1)
-    if file_name:
-        write_bmad(P, file_name, p0c = data_bunch1["p0c"][0])
-        print('Wrote Bmad beam file')
-    return P
 
 
 def get_tao_output_variables(tao:Tao) ->dict[str, list[Any]]:
